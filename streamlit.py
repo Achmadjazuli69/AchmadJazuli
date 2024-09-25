d8f7079fd2cb2635d0a91f1510982438fd77cc8e
Achmadjazuli69 <ach.jazz69@gmail.com>
2024-09-25 09:56:17 +0700

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+x = st.number_input("masukkan  suhu")
+sx = st.text_input("Satuan", "C")
+st.write("Anda menginput suhu ", x,"dengan satuan ", sx)
+sy = st.text_input("konversi ke", "C")
+if (sx == "C"):
+  pass
+else:
+  pass
+
+st.write("Hasil konversi dari ", x,sx,"adalah ...", sy)
