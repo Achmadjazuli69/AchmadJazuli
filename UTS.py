d037801ea2a8d11a592c1b8ee722d8dabab90efb
Achmadjazuli69 <ach.jazz69@gmail.com>
2024-10-24 09:22:35 +0700

    Update UTS.py

--- UTS.py
+++ UTS.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.patches import Circle
+from matplotlib.patches import Circle 
 
 
 st.title("Fisika Komputasi Awan")
